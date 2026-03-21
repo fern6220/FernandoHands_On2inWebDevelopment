bec3414b3a113e083eeef91596bfab7d23147a7c
fern6220 <obetfernando6220@gmail.com>
2026-03-21 19:16:35 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//Seletors
+//Navigation Bar 
+const title = document.getElementById("title1");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const interest = document.getElementById("interests");
+
+const navbar = document.getElementById("navbar");
+
+// navbar.addEventListener("click", )
+navbar.addEventListener("click", () => {
+    alert("Continue if POGI yung may ari");
+});
+
+//Footer
+const fb = document.getElementById("bfNiObet");
+const gmail = document.getElementById("gmailNiObet");
+
+foot.addEventListener("click", () =>{
+    alert("are you sure to go to this website?");
+});
